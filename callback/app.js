0de74c51df7c7fdf7f5e68f3f6fa47e0a46f160d
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-02 12:36:08 +0500

    call back

---
+++ callback/app.js
@@ -0,0 +1,11 @@
+ function sum(q,w) {
+     console.log(q+w)    
+ }
+function callback(a,b,sumcallback){
+    sumcallback(a,b)
+}
+callback(1,2,sum)
+const hello = ()=>{
+    console.log("hello")
+}
+setTimeout(hello , 2000);
